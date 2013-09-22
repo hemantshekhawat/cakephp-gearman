291dfc207301cb2e97c9627eb14b60f470153abf
davidsteinsland <david@davidsteinsland.net>
2013-09-22 18:57:30 +0200

    Fixed syntax errors

--- Test/Case/Console/Command/Task/GearmanShellTaskTest.php
+++ Test/Case/Console/Command/Task/GearmanShellTaskTest.php
@@ -65,6 +65,6 @@ class GearmanShellTaskTest extends CakeTestCase {
 		$class = new ReflectionClass('GearmanShellTask');
 		$property = $class->getProperty($property);
 		$property->setAccessible(true);
-		return $property->getValue();
+		return $property->getValue(new GearmanShellTask);
 	}
 }
